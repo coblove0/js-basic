08b58935c0a34cd7c6cc84a944f83ebb303e4965
coblove0 <coblove0@gmail.com>
2025-08-25 22:06:52 +0400

    6-driver

---
+++ 6-driver/app.js
@@ -0,0 +1,9 @@
+const hasLicense = true;
+const age = 32;
+const isDrunk = true;
+
+if (hasLicense && (age > 18) && !isDrunk) {
+    console.log('За руль можно!');
+} else {
+    console.log('За руль нельзя :(');
+}
