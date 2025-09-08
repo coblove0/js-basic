76a4d86f8554f9ba502ac9107a6f162def426f6e
unknown <present@d152w10.comp.npo>
2025-09-08 21:42:27 +0700

    поставлены <= вместо строгого <

--- 11-date-array/app.js
+++ 11-date-array/app.js
@@ -10,7 +10,7 @@ function RealDates (datesArray) {
       const dateStringArray = date.split('/');
       if (dateStringArray.length === 3 
           && (dateStringArray[0] > 0 && dateStringArray[0] <= 12)
-          && (dateStringArray[1] > 0 &&dateStringArray[1] < 31)
+          && (dateStringArray[1] > 0 &&dateStringArray[1] <= 31)
           && (dateStringArray[2] > 0 && dateStringArray[2] < 2060)) {
             result.push(`${dateStringArray[1]}-${dateStringArray[0]}-${dateStringArray[2]}`);
       }
@@ -18,7 +18,7 @@ function RealDates (datesArray) {
       const dateStringArray = date.split('-');
       if (dateStringArray.length === 3 
           && (dateStringArray[0] > 0 && dateStringArray[0] <= 31)
-          && (dateStringArray[1] > 0 &&dateStringArray[1] < 12)
+          && (dateStringArray[1] > 0 &&dateStringArray[1] <= 12)
           && (dateStringArray[2] > 0 && dateStringArray[2] < 2060)) {
             result.push(date);
       }
