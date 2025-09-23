98fb787bbe7fa6f9643fe34658b2be24cfcf4f73
coblove0 <present@d152w10.comp.npo>
2025-09-23 11:27:25 +0700

    15-tasks

---
+++ 15-tasks/app.js
@@ -0,0 +1,55 @@
+const ToDoList = {
+  tasks: [
+    {
+       title: 'Помыть посуду',
+       id: 1,
+       priority: 1
+    }
+  ],
+  Create: function (title, priority) {
+    if (!title) {
+      return false;
+    }
+    this.tasks.push({
+        title: title,
+        priority: priority ? priority : 0,
+        id: this.GetLastId() + 1
+      });
+      return true;
+  },
+  Update: function (id, title, priority) {
+    for (const taskInTasks of this.tasks) {
+      if (taskInTasks.id === id) {
+        taskInTasks.title = title ? title : taskInTasks.title;
+        taskInTasks.priority = priority ? priority : taskInTasks.priority;
+      }
+    }
+  },
+  Delete: function (id) {
+    const taskToDeleteIndex = this.tasks.findIndex(task => task.id === id);
+    this.tasks.splice(taskToDeleteIndex, 1);
+  },
+  Sort: function () {
+    this.tasks.sort((a, b) => a.priority - b.priority);
+  },
+  GetLastId: function () {
+    return this.tasks.reduce((acc,val) => acc = val.id, 0);
+  },
+  Get: function (id) {
+    return this.tasks.find(task => task.id === id);
+  }
+}
+
+console.log(ToDoList.tasks);
+ToDoList.Create('Проснуться');
+console.log(ToDoList.tasks);
+ToDoList.Create('Попить чай', 2);
+console.log(ToDoList.tasks);
+ToDoList.Create('Умыться', 1);
+console.log(ToDoList.tasks);
+ToDoList.Update(3, '', 3);
+console.log(ToDoList.tasks);
+ToDoList.Delete(1);
+console.log(ToDoList.tasks);
+ToDoList.Sort();
+console.log(ToDoList.tasks);
