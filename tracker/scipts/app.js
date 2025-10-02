3817ebfddee6f54340c2919189c9d0bd84879d0c
coblove0 <present@d152w10.comp.npo>
2025-10-02 23:01:25 +0700

    учеба окончена

--- tracker/scipts/app.js
+++ tracker/scipts/app.js
@@ -15,6 +15,10 @@ const page = {
   content: {
     daysContainer: document.getElementById('days'),
 		nextDay: document.querySelector('.habit__day')
+  },
+  popup: {
+    index: document.querySelector('.cover'),
+    iconField: document.querySelector('.popup__form input[name="icon"]')
   }
 }
 
@@ -30,6 +34,35 @@ function loadData () {
 
 function saveData () {
   localStorage.setItem(HABIT_KEY, JSON.stringify(habits));
+}
+
+function validateAndGetFormData(form, fields) {
+  const formData = new FormData(form);
+  const res = {};
+  for (const field of fields) {
+    const fieldValue = formData.get(field);
+    form[field].classList.remove('error');
+    if (!fieldValue) {
+      form[field].classList.add('error');
+    }
+    res[field] = fieldValue;
+  }
+  let isValid = true;
+  for (const field of fields) {
+    if (!res[field]) {
+      isValid = false;
+    }
+  }
+  if (!isValid) {
+    return;
+  }
+  return res;
+}
+
+function resetForm(form, fields) {
+  for (const field of fields) {
+    form[field].value = '';
+  }
 }
 
 /* render */
@@ -88,6 +121,7 @@ function rerender(activeHabitId) {
   if (!activeHabit) {
     return;
   }
+  document.location.replace(document.location.pathname + '#' + activeHabitId)
   rerenderMenu(activeHabit);
   rerenderHead(activeHabit);
   rerenderContent(activeHabit);
@@ -95,24 +129,22 @@ function rerender(activeHabitId) {
 
 /* work with days */
 function addDays(event) {
-  const form = event.target;
   event.preventDefault();
-  const data = new FormData(form);
-  const comment = data.get('comment');
-  form['comment'].classList.remove('error');
-  if (!comment) {
-    form['comment'].classList.add('error');
+
+  const data = validateAndGetFormData(event.target, ['comment']);
+  if (!data) {
+    return;
   }
   habits = habits.map(habit => {
     if (habit.id === globalActiveHabitId) {
       return {
         ...habit,
-        days: habit.days.concat([{ comment }])
+        days: habit.days.concat([{ comment: data.comment }])
       };
     }
     return habit;
   });
-  form['comment'].value = '';
+  resetForm(event.target, ['comment']);
   rerender(globalActiveHabitId);
   saveData();
 }
@@ -132,9 +164,52 @@ function deleteDay(dayIndex) {
   saveData();
 }
 
+/* popup */
+function togglePopup() {
+  const coverHiddenClass = 'cover_hidden';
+  if (page.popup.index.classList.contains(coverHiddenClass)) {
+    page.popup.index.classList.remove(coverHiddenClass);
+  } else {
+    page.popup.index.classList.add(coverHiddenClass);
+  }
+}
+
+/* working with habits */
+function setIcon(context, icon) {
+  page.popup.iconField.value = icon;
+  const activeIcon = document.querySelector('.icon.icon_active');
+  activeIcon.classList.remove('icon_active');
+  context.classList.add('icon_active');
+}
+
+function addHabit(event) {
+  event.preventDefault();
+  const data = validateAndGetFormData(event.target, ['name', 'icon', 'target']);
+  if (!data) {
+    return;
+  }
+  const maxId = habits.reduce((acc, habit) => acc > habit.id ? acc : habit.id, 0);
+  habits.push({
+    id: maxId + 1,
+    name: data.name,
+    target: data.target,
+    icon: data.icon,
+    days: []
+  });
+  resetForm(event.target, ['name', 'target']);
+  rerender(maxId + 1);
+  saveData();
+  togglePopup();
+}
 
 /* init */
 (() => {
   loadData();
-  rerender(habits[0].id);
+  const hashId = Number(document.location.hash.replace('#', ''));
+  const urlHabit = habits.find(habit => habit.id === hashId);
+  if (urlHabit) {
+    rerender(urlHabit.id);
+  } else {
+    rerender(habits[0].id);
+  }
 })();
